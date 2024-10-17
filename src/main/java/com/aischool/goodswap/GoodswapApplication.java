95226709e2c99481daf32a94ef7750d261971e0d
방찬혁 <cksgur2297@gmail.com>
2024-10-17 15:50:49 +0900

    first commit

---
+++ src/main/java/com/aischool/goodswap/GoodswapApplication.java
@@ -0,0 +1,13 @@
+package com.aischool.goodswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodswapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aischool/goodswap/GoodswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aischool.goodswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
