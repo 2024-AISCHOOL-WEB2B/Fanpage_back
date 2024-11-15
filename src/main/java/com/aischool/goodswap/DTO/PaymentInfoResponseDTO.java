f88baa7bd831091e4a0be3ee75fec9ae5e9fd134
방찬혁 <cksgur2297@gmail.com>
2024-11-15 18:38:40 +0900

    Refactor: PaymentInfoResponseDTO 구조 개선
      - 기존 필드들을 AddressDTO, CardInfoDTO, GoodsDTO로 분리하여 더 깔끔하고 확장 가능한 구조로 변경
      - PaymentInfoResponseDTO에서 주소, 카드 정보, 상품 정보를 각 DTO로 캡슐화하여 가독성 및 유지보수 용이성 향상

--- src/main/java/com/aischool/goodswap/DTO/PaymentInfoResponseDTO.java
+++ src/main/java/com/aischool/goodswap/DTO/PaymentInfoResponseDTO.java
@@ -1,40 +1,18 @@
 package com.aischool.goodswap.DTO;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
+@Builder
 public class PaymentInfoResponseDTO {
     
     private String user;
 
     private int point;
 
-    private String deliveryAddr;
+    private AddressDTO address;
+    private CardInfoDTO cardInfo;
+    private GoodsDTO goods;
 
-    private String cardNumber;
-    private String expiredAt;
-    private String cardCvc;
-
-    private String goodName;
-    private int goodsPrice;
-    private int shippingFee;
-
-    @Builder
-    public PaymentInfoResponseDTO(String user, int point, String deliveryAddr, String cardNumber, String expiredAt, String cardCvc, String goodName, int goodsPrice, int shippingFee) {
-        this.user = user;
-        this.point = point;
-        this.deliveryAddr = deliveryAddr;
-        this.cardNumber = cardNumber;
-        this.expiredAt = expiredAt;
-        this.cardCvc = cardCvc;
-        this.goodName = goodName;
-        this.goodsPrice = goodsPrice;
-        this.shippingFee = shippingFee;
-    }
 }
